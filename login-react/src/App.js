12d4203322060d5715ad0a06c3d9e7f0129560aa
nina <nina10313@gmail.com>
2020-11-18 21:52:42 +0800

    react jsx

---
+++ login-react/src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+function Word() {
+  return (
+    <div className="string">
+    <h2>登入</h2>
+    <h3>使用您的Google帳戶</h3>
+</div>
+  )
+}
+
+// export default App;
+export default Word;
