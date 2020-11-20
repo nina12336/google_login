1bff6919ef16f973138734cface7b57a9cd538ef
nina <nina10313@gmail.com>
2020-11-21 00:50:42 +0800

    css fiting

--- login-react/src/App.js
+++ login-react/src/App.js
@@ -1,35 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import './login.css';
+import logoimg from './Google.jpg';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
-function Word() {
+function App() {
   return (
-    <div className="string">
+<div className="container">
+  <div className="loginArea">
+  <div className="logo">
+            <img src={logoimg}/>
+        </div>
+  <div className="string">
     <h2>登入</h2>
     <h3>使用您的Google帳戶</h3>
+  </div>
+
+<input className="emailInput"  
+placeHolder="電子郵件地址或電話號碼" 
+required  type="text">
+</input>
+<br></br>
+<a href="#" className="forget">忘記電子郵件地址？</a>
+
+<p className="string2">如果這不是你的電腦，請使用訪客模式以私密方式登入。<a href="#" className="know">暸解詳情</a></p>
+
+
+<div className="click1">
+        <a href="#" className="create">建立帳戶</a>
+        <button className="continue" type="button">繼續</button>
 </div>
-  )
+</div>
+
+    <div className="click2">
+        <div className="language">
+          <select className="list">
+            <option value="" selected >繁體中文</option>
+            <option>英文</option>
+          </select>
+       </div>
+    
+    <div className="btn">
+    <button>說明</button>
+    <button>隱私</button>
+    <button>條款</button>
+    </div>
+
+   </div>
+
+
+
+</div>
+  );
 }
 
-// export default App;
-export default Word;
+export default App;
--- login-react/src/index.js
+++ login-react/src/index.js
@@ -1,56 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import Word from './App';
+// import './login.css';
+import App from './App'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <div>
-  <div className="string">
-    <h2>登入</h2>
-    <h3>使用您的Google帳戶</h3>
-  </div>
-
-<div>
-<input className="emailInput"  
-placeHolder="電子郵件地址或電話號碼" 
-required  type="text">
-</input>
-
-<a href="#" className="forget">忘記電子郵件地址？</a>
-
-<p className="string2">如果這不是你的電腦，請使用訪客模式以私密方式登入。<a href="#" className="know">暸解詳情</a></p>
-</div>
-
-<div class="click1">
-        <a href="#" class="create">建立帳戶</a>
-        <button class="continue" type="button">繼續</button>
-</div>
-
-    <div class="click2">
-        <div class="language">
-          <select class="list">
-            <option value="" selected >繁體中文</option>
-            <option>英文</option>
-          </select>
-       </div>
-    
-    <div class="btn">
-    <button>說明</button>
-    <button>隱私</button>
-    <button>條款</button>
-    </div>
-
-   </div>
-
-
-
-</div>,
-  // <React.StrictMode>
-  //   <App />
-  //   <Word />
-  // </React.StrictMode>,
+  
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
